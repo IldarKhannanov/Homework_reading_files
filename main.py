28c3e894cf7f555ccd9c6a4ada1b6c564c908b21
Ildar Khannanov <ildark1582@gmail.com>
2023-06-04 15:59:10 +0500

    zadacha_2

--- main.py
+++ main.py
@@ -16,4 +16,22 @@ with open('recipes.txt', 'r', encoding = 'utf-8') as f:
 import json
 
 a = json.dumps(cook_book, indent=2, ensure_ascii=False)
-print(a)
+
+def get_shop_list_by_dishes(person_count: int, dishes: list):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for consist in cook_book[dish]:
+                if consist['ingridient_name'] in result:
+                    result[consist['ingridient_name']]['quantity'] += consist['quantity'] * person_count
+                else:
+                    result[consist['ingridient_name']] = {'measure': consist['measure'],'quantity': (consist['quantity'] * person_count)}
+        else:
+            print('This dish is not in the cook book')
+    print(result)
+get_shop_list_by_dishes(2, ['Запеченный картофель', 'Омлет'])
+
+
+
+
+
